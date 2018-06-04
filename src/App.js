7558a9e994df16ef45c048923be7831fba8cbbc1
Troy Wu <pightmp@gmail.com>
2018-06-04 18:22:57 +0800

    feat: complete react version

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import shopData from './data';
 
 class App extends Component {
   render() {
+    const shopDetail = ({ match }) => {
+      const shopKey = match.params.shop;
+      const shop = shopData[shopKey];
+      return (
+        <div id={shopKey} className="item">
+          <h1>{shop.chtName}</h1>
+          <p>{shop.bnhName}: <a href={`tel:${shop.tel}`}>{shop.tel}</a></p>
+          <div className="menu">
+            <img src={shop.menuSrc} />
+          </div>
+        </div>
+      );
+    };
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div>
+          <nav className="navigator">
+            <ul>
+              {Object.keys(shopData).map(shopKey => (
+                <li><Link to={'/' + shopKey}>{shopData[shopKey].chtName}</Link></li>
+              ))}
+            </ul>
+          </nav>
+          <div className="stage">
+            <Route path="/:shop" component={shopDetail} />
+          </div>
+        </div>
+      </Router>
     );
   }
 }
